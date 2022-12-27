13c5e560f87a118b2e16bed3c7f72b5134c4baac
Jongheon Kim <simba3447@gmail.com>
2022-12-27 11:34:53 +0900

    Add answer for baekjoon 5568

---
+++ baekjoon/5568/5568.py
@@ -0,0 +1,23 @@
+n = int(input())
+k = int(input())
+card_list = []
+for i in range(n):
+    card_list.append(input())
+
+def count_integer(card_list, k):
+    integer_list = []
+    def count_integer_rec(card_list, k, integer):
+        if k == 0:
+            if integer not in integer_list:
+                integer_list.append(integer)
+
+        for i in range(len(card_list)):
+            card = card_list[i]
+            rest_card_list = card_list[:i] + card_list[i+1:]
+            count_integer_rec(rest_card_list, k-1, integer + card)
+
+    count_integer_rec(card_list, k, '')
+    integer_list.sort()
+    print(len(integer_list))
+
+count_integer(card_list, k)
